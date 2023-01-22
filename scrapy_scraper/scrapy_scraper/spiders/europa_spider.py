aaad9441a3013539cd10e61f2c2bd077b8802f56
Louis Münzner <munzner.louis@gmail.com>
2023-01-23 00:26:51 +0100

    nouvelle version utilisant le tableau

--- scrapy_scraper/scrapy_scraper/spiders/europa_spider.py
+++ scrapy_scraper/scrapy_scraper/spiders/europa_spider.py
@@ -1,44 +1,54 @@
-#La V2 utilise la page "print" permettant d'afficher toute les pages sur un seul url, evitant donc de faire une nouvelle requête pour chaque changement de page
-#Moins de requêtes => site moins saturé + Scraper plus rapide => proprio du site content + jury content => mieux
-#par rapport a beautifulsoup, on fait qu'une seul requête par pays, donc 33 requêtes asynchrones, contre ~1600 requêtes (pas asynchrone) avant => mieux
 
-import asyncio      #pip install asyncio    
-import requests  #pas sûre si on l'utilisera
+import asyncio      #pip install asyncio (pas sûre de l'utiliser)    
 import scrapy       #pip install scrapy  --> scrapy ver. > 2.4 pour utiliser asyncio
 
 class europa_spider(scrapy.Spider):
 
     name = "europa_spider"
     start_urls = [
-        "https://ec.europa.eu/clima/ets/oha.do?form=oha&languageCode=fr&account.registryCodes=AT&accountHolder=&installationIdentifier=&installationName=&permitIdentifier=&mainActivityType=-1&searchType=oha&currentSortSettings=&resultList.currentPageNumber=2&print=Print"
+        "https://ec.europa.eu/clima/ets/oha.do?form=oha&languageCode=fr&account.registryCodes=AT&accountHolder=&installationIdentifier=&installationName=&permitIdentifier=&mainActivityType=-1&searchType=oha&currentSortSettings=&resultList.currentPageNumber=1&nextList=Next%3E"
     ]
-    def start_requests(self):  #override de la fonction start_request pour que la premiere fonction lancé soit 'parse_countries' et pas 'parse' 
-        yield scrapy.Request("https://ec.europa.eu/clima/ets/oha.do?form=oha&languageCode=fr&account.registryCodes=AT&accountHolder=&installationIdentifier=&installationName=&permitIdentifier=&mainActivityType=-1&searchType=oha&currentSortSettings=&resultList.currentPageNumber=2&print=Print", callback=self.parse_countries)
+
+    async def parse(self, response): #extrait les données du tableau
+        for row in response.css('table#tblAccountSearchResult tr:nth-child(n+3)'):  #on commande a n+3 car les infos sont pas pertinentes avant ça
+
+            dico_data ={                'National_Administrator': row.css('td:nth-child(1) span::text').get().strip(),
+                'Account_Type': row.css('td:nth-child(2) span::text').get().strip(),
+                'Account_Holder_Name': row.css('td:nth-child(3) span::text').get().strip(),
+                'Installation/Aircraft_ID': row.css('td:nth-child(4) span::text').get().strip(),
+                'Installation_Name/Aircraft_Operator_Code': row.css('td:nth-child(5) span::text').get().strip(),
+                'Company_Regustration_No': row.css('td:nth-child(6) span::text').get().strip(),
+                'Permit/Plan_ID': row.css('td:nth-child(7) span::text').get().strip(),
+                'Permit/Plan_Date': row.css('td:nth-child(8) span::text').get().strip(),
+                'Main_Activity_Type': row.css('td:nth-child(9) span::text').get().strip(),
+                'Latest_Compliance_Code': row.css('td:nth-child(10) span::text').get().strip()} 
+                #ajouter une colonne par année de Compliance "Compliance_2005 , Compliance_2006" etc avec des tuples pour les données des lignes
+
+            yield dico_data
+            
 
     async def parse_countries(self, response):
-        # Extract the list of countries from the form
-        countries = response.xpath("//table[@id='tblAccountSearchCriteria']//select[@name='account.registryCodes']/option/@value").extract()
-        print("CHECKPOINT 1",countries,"TEST",countries[1])
-        for country in countries:
-            url=f"https://ec.europa.eu/clima/ets/oha.do?form=oha&languageCode=fr&account.registryCodes={country}&accountHolder=&installationIdentifier=&installationName=&permitIdentifier=&mainActivityType=-1&searchType=oha&currentSortSettings=&resultList.currentPageNumber=2&print=Print"
-            print("le pays scrapé est :",country,"avec pour url :\n",url)
+        return
+        #to be coded
 
-            yield scrapy.Request(url,callback=self.parse)
-            
-    async def parse(self, response): #extrait les données du tableau
-        
-        accounts = response.xpath('/html/body/ul[2]/li')
-        for account in accounts:
-            data_dico ={}
-            for li in account.xpath('.//ul/li'):
-                key = li.xpath(".//span[@class='key']/text()").get()
-                value = li.xpath(".//span[@class='value']/text()").get()
-                data_dico[key]= value
-            yield data_dico
+    async def parse_pages(self, response):
+        return
+        #to be coded
+
+    async def parse_Compliances(self, response):
+        return
+        #to be coded
 
 
+'''
+lignes de commande pour lancer scrapy / store data dans un cmd : 
+scrapy crawl europa_spider -O data.csv                 
+scrapy runspider europa_spider.py -O data.csv
 
-#lignes de commande pour lancer scrapy / store data dans un cmd : 
-# en Json :      scrapy crawl europa_spiderV2 -O data.json                 O majuscule pour Override au cas ou il y a deja un fichier data.json
-# en csv  :      scrapy crawl europa_spiderV2 -o data.csv                  O majuscule fonctionne pas pour override en format csv
-#il en existe surement plus
+"runspider" permet de lancer une spider à la fois, alors que "crawl" peut permettre d'en lancer plusieurs
+
+pour lancer Crawl il faut etre n'importe ou dans le projet scrapy
+
+pour lancer runspider il faut être dans le meme directory que la spider (ici europa_spider) 
+c'est pour ça que le .csv se sauvegarde automatiquement a cet endroit
+'''
