7de23d1d251f37ea8a9c6f845f7ac1e421a78995
Louis Münzner <munzner.louis@gmail.com>
2023-01-27 23:05:04 +0100

    fix des erreurs de scraping (issues 1 et 2)

--- scrapy_scraper/scrapy_scraper/spiders/europa_spider.py
+++ scrapy_scraper/scrapy_scraper/spiders/europa_spider.py
@@ -9,20 +9,23 @@ class europa_spider(scrapy.Spider):
     ]
 
     def start_requests(self):  #override de la fonction start_request pour que la premiere fonction lancé soit 'parse_countries' et pas 'parse' 
-        yield scrapy.Request("https://ec.europa.eu/clima/ets/oha.do?form=oha&languageCode=fr&account.registryCodes=AT&accountHolder=&installationIdentifier=&installationName=&permitIdentifier=&mainActivityType=-1&searchType=oha&currentSortSettings=&resultList.currentPageNumber=1&nextList=Next%3", callback=self.parse_countries_and_pages)
+        yield scrapy.Request("https://ec.europa.eu/clima/ets/oha.do?form=oha&languageCode=fr&account.registryCodes=AT&accountHolder=&installationIdentifier=&installationName=&permitIdentifier=&mainActivityType=-1&searchType=oha&currentSortSettings=&resultList.currentPageNumber=1&nextList=Next%3", callback=self.parse_countries)
 
-    async def parse_countries_and_pages(self, response):
+    async def parse_countries(self, response):
         countries = response.xpath("//table[@id='tblAccountSearchCriteria']//select[@name='account.registryCodes']/option/@value").extract()
         for country in countries:
-            pages = response.css("td.bgpagecontent input:nth-child(5)::attr(value)").get()
-            for page in range(int(pages)):
-                url=f"https://ec.europa.eu/clima/ets/oha.do?form=oha&languageCode=fr&account.registryCodes={country}&accountHolder=&installationIdentifier=&installationName=&permitIdentifier=&mainActivityType=-1&searchType=oha&currentSortSettings=&resultList.currentPageNumber={page}&nextList=Next%3"
-                yield scrapy.Request(url,callback=self.parse)        
+            url = f"https://ec.europa.eu/clima/ets/oha.do?form=oha&languageCode=fr&account.registryCodes={country}&accountHolder=&installationIdentifier=&installationName=&permitIdentifier=&mainActivityType=-1&searchType=oha&currentSortSettings=&resultList.currentPageNumber=1&nextList=Next%3"
+            yield scrapy.Request(url, callback=self.parse_pages, meta={'country': country})       
+
+    async def parse_pages(self,response):
+        pages = response.css("td.bgpagecontent input:nth-child(5)::attr(value)").get()
+        country = response.meta['country']
+        for page in range(int(pages)):
+            url = f"https://ec.europa.eu/clima/ets/oha.do?form=oha&languageCode=fr&account.registryCodes={country}&accountHolder=&installationIdentifier=&installationName=&permitIdentifier=&mainActivityType=-1&searchType=oha&currentSortSettings=&resultList.currentPageNumber={page}&nextList=Next%3"
+            yield scrapy.Request(url, callback=self.parse)
 
     async def parse(self, response): #extrait les données du tableau
-
         for row in response.css('table#tblAccountSearchResult tr:nth-child(n+3)'):  # (n+3) car les infos sont pas pertinentes avant ça
-            
             dico_table_data ={               
                 'National_Administrator':                   None if not row.css('td:nth-child(1) span::text').get().strip() else row.css('td:nth-child(1) span::text').get().strip(),
                 'Account_Type':                             None if not row.css('td:nth-child(2) span::text').get().strip() else row.css('td:nth-child(2) span::text').get().strip(),
@@ -35,10 +38,8 @@ class europa_spider(scrapy.Spider):
                 'Main_Activity_Type':                       None if not row.css('td:nth-child(9) span::text').get().strip() else row.css('td:nth-child(9) span::text').get().strip(),
                 'Latest_Compliance_Code':                   None if not row.css('td:nth-child(10) span::text').get().strip() else row.css('td:nth-child(10) span::text').get().strip()} 
             url = row.css('td:nth-child(11) td:nth-child(2) a::attr(href)').get()
-
             yield scrapy.Request(url, self.parse_compliances,meta={'dico_table_data': dico_table_data})
-
-
+            #permet de passer le dico en paramètre pour la fonction parse_compliance
 
     async def parse_compliances(self, response):
         dico_table_data = response.meta['dico_table_data']        
@@ -57,7 +58,8 @@ class europa_spider(scrapy.Spider):
 
 '''
 lignes de commande pour lancer scrapy / store data dans un cmd : 
-scrapy crawl europa_spider -O data.csv                 
+scrapy crawl europa_spider -O data.csv    
+ou             
 scrapy runspider europa_spider.py -O data.csv
 
 "runspider" permet de lancer une spider à la fois, alors que "crawl" peut permettre d'en lancer plusieurs
